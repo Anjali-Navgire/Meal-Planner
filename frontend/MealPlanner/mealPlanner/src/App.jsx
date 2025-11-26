896f97b338a4c2de504158bca4fe308aa05c008d
Anjali-Navgire <anjalinavgire3109@gmail.com>
2025-11-26 21:27:25 +0530

    fixed file name

--- frontend/MealPlanner/mealPlanner/src/App.jsx
+++ frontend/MealPlanner/mealPlanner/src/App.jsx
@@ -5,16 +5,13 @@ import Login from "./components/Login";
 import Home from "./pages/Home";
 import CreateRecipe from "./pages/CreateRecipe";
 import Favorites from "./pages/Favorites";
-import WeeklyMealPlanner from "./pages/weeklyMealPlanner";
-
-
+import WeeklyMealPlanner from "./pages/WeeklyMealPlanner";
 
 import ProtectedRoute from "./components/ProtectedRoute"
 function App() {
 
   return (
     <>
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Signup />} />
@@ -25,9 +22,7 @@ function App() {
           <Route path="/weekly" element={<WeeklyMealPlanner />} />
         </Routes>
       </BrowserRouter>
-
     </>
-
   )
 }
 
